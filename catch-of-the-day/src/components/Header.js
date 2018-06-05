f504bd628b3bc1825c8c026eb07fecd95e2bbe12
RISTO TAPANI RAIHALA <tyokone@RISTOs-MBP.home>
2018-06-05 17:06:06 +0200

    addFish state

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render () {
 
-        return (
-            <header className="top">
+const Header = props => (
+    <header className="top">
                 <h1>Catch 
                     <span className="ofThe">
                     <span className="of">Of</span>
@@ -13,13 +11,33 @@ class Header extends React.Component {
                     Day
                     </h1>
             <h3 className="tagline">
-            <span>Fresh daily</span>
+            <span>{props.tagline}</span>
             </h3>
-            </header>
+            </header> 
+)
 
-        )
+
+
+// class Header extends React.Component {
+//     render () {
+
+//         return (
+//             <header className="top">
+//                 <h1>Catch 
+//                     <span className="ofThe">
+//                     <span className="of">Of</span>
+//                     <span className="the">The</span>
+//                     </span>
+//                     Day
+//                     </h1>
+//             <h3 className="tagline">
+//             <span>{this.props.tagline}</span>
+//             </h3>
+//             </header>
+
+//         )
     
-    }
-}
+//     }
+// }
 
 export default Header;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+    myInput=React.createRef();
+
+    goToStore = (event) => {
+        event.preventDefault();
+        
+        const storeName = this.myInput.value.value;
+
+        this.props.history.push(`/store/${this.storeName}`);
+    }
+
     render (){
       return (
          
          
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
-          <input type="text" required placeholder="Store Name"/>
+          <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue= { getFunName()}/>
           <button type="submit">Visit store</button>
       </form>
 
