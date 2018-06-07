fdf98d1754bd4ef3a3931533194226788ea79704
RISTO TAPANI RAIHALA <tyokone@RISTOs-MBP.home>
2018-06-07 17:43:25 +0200

    delete fish added

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -53,6 +53,15 @@ class App extends React.Component {
         this.setState({ fishes });
     }
 
+    deleteFish = (key) => {
+        //1. Take a copy of state
+        const fishes = { ...this.state.fishes };
+        //2. update the state
+        fishes[key]= null;
+        //3.update state
+        this.setState({ fishes });
+    }
+
     loadSampleFishes=()=>{
         this.setState({
             fishes: sampleFishes
@@ -67,6 +76,18 @@ class App extends React.Component {
     //3. call setstate to update our state object
     this.setState({ order });
     };
+
+    removeFromOrder = (key) => {
+         //1.take copy of state
+    const order = {...this.state.order};
+    //2. remove that item from order
+    delete order[key];
+    //3. call setstate to update our state object
+    this.setState({ order });
+    }
+
+
+
     render () {
         return (
             <div className="catch-of-the-day">
@@ -78,10 +99,14 @@ class App extends React.Component {
 
                     </ul>
                 </div>
-                <Order fishes={this.state.fishes} order={this.state.order} /> 
+                <Order 
+                fishes={this.state.fishes} 
+                order={this.state.order} 
+                removeFromOrder={this.removeFromOrder}/> 
                 <Inventory 
                 addFish={this.addFish} 
                 updateFish={this.updateFish} 
+                deleteFish={this.deleteFish}
                 loadSampleFishes={this.loadSampleFishes}
                 fishes={this.state.fishes}
                 />
